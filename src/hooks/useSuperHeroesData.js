111dcd9c05676ee3779ec00cecbd24e805e126a1
Tomasz Stanisz <fremen1990@gmail.com>
2022-11-21 21:49:53 +0100

    React Query Tutorial - 22 - Query Invalidation

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 
 const fetchSuperHeroes = () => {
@@ -28,5 +28,10 @@ const addSuperHero = (hero) => {
 };
 
 export const useAddSuperHeroData = () => {
-  return useMutation(addSuperHero);
+  const queryClient = useQueryClient();
+  return useMutation(addSuperHero, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("super-heroes");
+    },
+  });
 };
